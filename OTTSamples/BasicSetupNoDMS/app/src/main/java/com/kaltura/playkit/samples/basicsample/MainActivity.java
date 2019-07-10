f02b8d3ff2379a8cc6f14c9c71d4d785f528e2c7
Gilad Nadav <gilad.nadav@kaltura.com>
2019-07-10 18:54:02 +0300

    add BasicSampleNoDMS -> shoes how to run OTT incase DMS is not configured or no meidaprep

---
+++ OTTSamples/BasicSetupNoDMS/app/src/main/java/com/kaltura/playkit/samples/basicsample/MainActivity.java
@@ -0,0 +1,189 @@
+package com.kaltura.playkit.samples.basicsample;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.kaltura.playkit.PKLog;
+import com.kaltura.playkit.PlayerEvent;
+import com.kaltura.playkit.PlayerState;
+import com.kaltura.playkit.providers.api.phoenix.APIDefines;
+import com.kaltura.playkit.providers.ott.PhoenixMediaProvider;
+import com.kaltura.tvplayer.KalturaPlayer;
+import com.kaltura.tvplayer.OTTMediaOptions;
+import com.kaltura.tvplayer.PlayerInitOptions;
+import com.kaltura.tvplayer.config.PhoenixTVPlayerParams;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final PKLog log = PKLog.get("MainActivity");
+
+    public static final String SERVER_URL = "https://api-preprod.ott.kaltura.com/v5_1_0/";
+    public static final String MEDIA_FORMAT = "Mobile_Devices_Main_HD_Dash";
+    public static final int PARTNER_ID = 198;
+    private static final String ASSET_ID = "480989";
+
+    private static final Long START_POSITION = 0L; // position for start playback in msec.
+
+    private KalturaPlayer player;
+    private Button playPauseButton;
+    private boolean isFullScreen;
+    private PlayerState playerState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadPlaykitPlayer();
+
+        addPlayPauseButton();
+
+        showSystemUI();
+
+        (findViewById(R.id.activity_main)).setOnClickListener(v -> {
+            if (isFullScreen) {
+                showSystemUI();
+            } else {
+                hideSystemUI();
+            }
+        });
+
+    }
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
+        isFullScreen = true;
+    }
+
+    private void showSystemUI() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        isFullScreen = false;
+    }
+
+
+    /**
+     * Just add a simple button which will start/pause playback.
+     */
+    private void addPlayPauseButton() {
+        //Get reference to the play/pause button.
+        playPauseButton = this.findViewById(R.id.play_pause_button);
+        //Add clickListener.
+        playPauseButton.setOnClickListener(v -> {
+            if (player != null) {
+                if (player.isPlaying()) {
+                    //If player is playing, change text of the button and pause.
+                    playPauseButton.setText(R.string.play_text);
+                    player.pause();
+                } else {
+                    //If player is not playing, change text of the button and play.
+                    playPauseButton.setText(R.string.pause_text);
+                    player.play();
+                }
+            }
+        });
+    }
+
+    private void addPlayerStateListener() {
+        player.addListener(this, PlayerEvent.stateChanged, event -> {
+            log.d("State changed from " + event.oldState + " to " + event.newState);
+            playerState = event.newState;
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null && playerState != null) {
+            if (playPauseButton != null) {
+                playPauseButton.setText(R.string.pause_text);
+            }
+            player.onApplicationResumed();
+            player.play();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.onApplicationPaused();
+        }
+    }
+
+    public void loadPlaykitPlayer() {
+        PlayerInitOptions playerInitOptions = new PlayerInitOptions(PARTNER_ID);
+        playerInitOptions.setAutoPlay(true);
+        playerInitOptions.setAllowCrossProtocolEnabled(true);
+
+        /*
+            NOTE:
+             In case of OTT Partner where DMS is not configured
+             playerInitOptions.tvPlayerParams
+             should be set manually Q/W playback will be blocked.
+             please verify what is your OVP PartnerId!!!
+         */
+
+        PhoenixTVPlayerParams phoenixTVPlayerParams = new PhoenixTVPlayerParams();
+        phoenixTVPlayerParams.analyticsUrl = "https://analytics.kaltura.com";
+        phoenixTVPlayerParams.ovpPartnerId = 1774581;
+        phoenixTVPlayerParams.partnerId = PARTNER_ID;
+        phoenixTVPlayerParams.serviceUrl = SERVER_URL;
+        phoenixTVPlayerParams.ovpServiceUrl = "http://cdnapi.kaltura.com/";
+        playerInitOptions.tvPlayerParams = phoenixTVPlayerParams;
+
+        player = KalturaPlayer.createOTTPlayer(MainActivity.this, playerInitOptions);
+        player.setPlayerView(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        ViewGroup container = findViewById(R.id.player_root);
+        container.addView(player.getPlayerView());
+
+        OTTMediaOptions ottMediaOptions = buildOttMediaOptions();
+        player.loadMedia(ottMediaOptions, (entry, loadError) -> {
+            if (loadError != null) {
+                Snackbar.make(findViewById(android.R.id.content), loadError.getMessage(), Snackbar.LENGTH_LONG).show();
+            } else {
+                log.d("OTTMedia onEntryLoadComplete  entry = " + entry.getId());
+            }
+        });
+
+        addPlayerStateListener();
+    }
+
+    private OTTMediaOptions buildOttMediaOptions() {
+        OTTMediaOptions ottMediaOptions = new OTTMediaOptions();
+        ottMediaOptions.assetId = ASSET_ID;
+        ottMediaOptions.assetType = APIDefines.KalturaAssetType.Media;
+        ottMediaOptions.contextType = APIDefines.PlaybackContextType.Playback;
+        ottMediaOptions.assetReferenceType = APIDefines.AssetReferenceType.Media;
+        ottMediaOptions.protocol = PhoenixMediaProvider.HttpProtocol.Https;
+        ottMediaOptions.ks = null;
+        ottMediaOptions.startPosition = START_POSITION;
+        ottMediaOptions.formats = new String []{MEDIA_FORMAT};
+
+        return ottMediaOptions;
+    }
+}
