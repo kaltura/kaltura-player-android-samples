a614001c74cd580a9bb60316bf21b5b57e17fa20
Gilad Nadav <gilad.nadav@kaltura.com>
2019-11-25 20:59:19 +0200

    remove widgetId -> moved to media config

--- AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/PlayerActivity.java
+++ AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/PlayerActivity.java
@@ -327,7 +327,6 @@ public class PlayerActivity extends AppCompatActivity implements Observer {
                 .setKs(appPlayerInitConfig.ks)
                 .setPreload(appPlayerInitConfig.preload)
                 .setReferrer(appPlayerInitConfig.referrer)
-                .setWidgetId(appPlayerInitConfig.widgetId)
                 .setAllowCrossProtocolEnabled(appPlayerInitConfig.allowCrossProtocolEnabled)
                 .setPreferredMediaFormat(appPlayerInitConfig.preferredFormat)
                 .setSecureSurface(appPlayerInitConfig.secureSurface)
