cddf27cc3ad254aa281ce6aefb6bf6bd4f2839b8
Gilad Nadav <gilad.nadav@kaltura.com>
2020-04-21 17:16:34 +0300

    add                        var playerVersion: String,                        var playerType: String,
    to sdkSettings

--- AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/models/ima/SdkSettings.kt
+++ AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/models/ima/SdkSettings.kt
@@ -3,4 +3,6 @@ package com.kaltura.kalturaplayertestapp.models.ima
 data class SdkSettings(var numRedirects: Int = 4,
                        var autoPlayAdBreaks: Boolean = true,
                        var debugMode: Boolean = false,
+                       var playerVersion: String,
+                       var playerType: String,
                        @Transient var language: String = "en")
