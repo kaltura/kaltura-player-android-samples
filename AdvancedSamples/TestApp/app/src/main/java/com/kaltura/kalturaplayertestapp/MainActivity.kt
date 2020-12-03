f4bc950c2646d2cab278416092fa8623691a2cbe
Gilad Nadav <gilad.nadav@kaltura.com>
2020-12-03 19:47:12 +0200

    fix build

--- AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/MainActivity.kt
+++ AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/MainActivity.kt
@@ -285,8 +285,10 @@ open class MainActivity: BaseActivity(), TestCaseConfigurationAdapter.OnJsonSele
             if (resultCode == CommonStatusCodes.SUCCESS) {
                 if (data != null) {
                     val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
-                    Log.d(TAG, "Barcode read: " + barcode.displayValue)
-                    loadTestsFromUrl(barcode.displayValue)
+                    Log.d(TAG, "Barcode read: " + barcode?.displayValue)
+                    if (barcode != null) {
+                        loadTestsFromUrl(barcode.displayValue)
+                    }
                 } else {
                     Log.d(TAG, "No barcode captured, intent data is null")
                 }
