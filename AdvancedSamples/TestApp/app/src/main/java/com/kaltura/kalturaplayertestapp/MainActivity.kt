2690752f8c64052337a9b1de89696e9155f0809a
GouravSna <38206744+GouravSna@users.noreply.github.com>
2020-12-07 14:32:17 +0530

    FEC-10492 Added Youbora Bundle Configuration in TestApp (#68)

--- AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/MainActivity.kt
+++ AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/MainActivity.kt
@@ -286,8 +286,8 @@ open class MainActivity: BaseActivity(), TestCaseConfigurationAdapter.OnJsonSele
                 if (data != null) {
                     val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
                     Log.d(TAG, "Barcode read: " + barcode?.displayValue)
-                    if (barcode != null) {
-                        loadTestsFromUrl(barcode.displayValue)
+                    barcode?.let {
+                        loadTestsFromUrl(it.displayValue)
                     }
                 } else {
                     Log.d(TAG, "No barcode captured, intent data is null")
--- AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/converters/PluginDescriptor.kt
+++ AdvancedSamples/TestApp/app/src/main/java/com/kaltura/kalturaplayertestapp/converters/PluginDescriptor.kt
@@ -5,5 +5,6 @@ import com.google.gson.JsonElement
 
 class PluginDescriptor {
     var pluginName: String? = null
+    var isBundle: Boolean? = false
     var params: JsonElement? = null
 }
